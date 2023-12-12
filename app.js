111f9c08ac1addba2d2930b8cdfaebce31a98200
Joseph Odeleye <141724134+Josewin01@users.noreply.github.com>
2023-12-13 00:31:58 +0100

    Added functionality to the 'NEW BUTTON'

--- app.js
+++ app.js
@@ -11,16 +11,7 @@ GAME RULES:
 
 var scores, roundScore, activePlayer;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-document.getElementById('score-0'). textContent = '0';
-document.getElementById('score-1'). textContent = '0';
-document.getElementById('current-0'). textContent = '0';
-document.getElementById('current-1'). textContent = '0';
-
-document.querySelector('.dice').style.display = 'none';
+init()
 // document.querySelector('#current-' + activePlayer).textContent = dice;
 
 
@@ -69,8 +60,32 @@ document.querySelector('.btn-hold').addEventListener('click', function(){
 
 })
 
+document.querySelector('.btn-new').addEventListener('click', function(){
+    init()
 
+    
+    document.querySelector('#name-0').textContent = 'Player 1'
+    document.querySelector('#name-1').textContent = 'Player 2'
+    
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+})
 
+function init(){
+    scores = [0,0];
+    roundScore = 0;
+    activePlayer = 0;
+
+    document.getElementById('score-0'). textContent = '0';
+    document.getElementById('score-1'). textContent = '0';
+    document.getElementById('current-0'). textContent = '0';
+    document.getElementById('current-1'). textContent = '0';
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 function nextPlayer() {
     activePlayer ===0 ? activePlayer = 1 : activePlayer = 0;
